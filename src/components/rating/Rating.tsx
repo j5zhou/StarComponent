56be1deacf3d8db9059bf2efa37a905c9db61ba0
Khoa Nguyen <nkhoa0101@yahoo.com>
2022-03-08 19:36:03 -0800

    Set up branch feature/star/size

---
+++ src/components/rating/Rating.tsx
@@ -0,0 +1,64 @@
+
+import React, { FC, useEffect, useState } from 'react';
+import "./rating.scss";
+import Star from './Star';
+
+export enum RatingSizeType {
+    Medium,
+    Small,
+    Large,
+}
+
+interface IRatingProps {
+    disabled?: boolean,
+    label?: string,
+    value?: number,
+    size?: RatingSizeType,
+    count?: number,
+    onChange?: (e?: React.MouseEvent) => void;
+}
+
+export default function Rating({ disabled = false, label, value = 4, size = RatingSizeType.Medium, count = 5, onChange }: IRatingProps) {
+    const [starsArray,setStarsArray]=useState<number[]>([]);
+    const [currentValue,setCurrentValue] = useState<number>(value);
+
+    useEffect(()=>{
+        changeStarArray(currentValue);
+    },[count,disabled,currentValue]);
+
+    useEffect(()=>{
+        setCurrentValue(value);
+    },[value])
+
+
+    const changeStarArray = (currValue:number)=>{
+        const newStarArr:number[] = [];
+        for(let i=1;i<=count;i++){
+            if(i<=currValue){
+                newStarArr.push(1);
+            }else{
+                newStarArr.push(Math.max(currValue-i+1,0))
+            }
+        }
+        setStarsArray(newStarArr);
+    }
+
+    const mouseEnterEvent = (index:number)=>{
+        changeStarArray(index);
+    }
+    const mouseLeaveEvent = ()=>{
+        changeStarArray(currentValue);
+    }
+    const mouseClickEvent = (index:number)=>{
+        setCurrentValue(index);
+    }
+
+    return <div className='Rating-Component medium'>
+        <div className='star-line'>
+            {
+                starsArray.map((item,index)=> <Star key={index} value={item} index={index} mouseEnterEvent={mouseEnterEvent} mouseLeaveEvent={mouseLeaveEvent} mouseClickEvent={mouseClickEvent} />)
+            }
+        </div>
+
+    </div>
+}
---
+++ src/components/rating/Star.tsx
@@ -0,0 +1,62 @@
+import { RatingSizeType } from "./Rating";
+import React, { FC, useEffect, useState } from 'react';
+import "./rating.scss";
+
+interface IStarProps{
+    disabled?: boolean,
+    value?: number,
+    size?: RatingSizeType,
+    index:number,
+    mouseEnterEvent:(ind:number) => void,
+    mouseLeaveEvent:() => void,
+    mouseClickEvent:(ind:number) => void,
+    onChange?: (e?: React.MouseEvent) => void;
+}
+
+export default function Star({ disabled = false, value = 0.5, size = RatingSizeType.Medium,index, onChange,mouseEnterEvent,mouseLeaveEvent,mouseClickEvent }: IStarProps){
+    console.log(index,value);
+    const activeLeftClassOptions:string[] = [];
+    const activeRightClassOptions:string[] = [];
+    const defaultClassOptions:string[] = [];
+    let starPoints:string = "";
+    switch(size){
+        case RatingSizeType.Medium:
+            activeLeftClassOptions.push("medium");
+            activeRightClassOptions.push("medium");
+            defaultClassOptions.push("medium");
+            starPoints = "12.5,1.25 5,24.75 23.75,9.75 1.25,9.75 20,24.75";
+            break;
+    }
+    switch(value){
+        case 0.5:
+            activeLeftClassOptions.push("fill");
+            break;
+        case 1:
+            activeLeftClassOptions.push("fill");
+            activeRightClassOptions.push("fill");
+            break;
+    }
+
+
+    return <div className='star-item' onMouseLeave={()=>mouseLeaveEvent()}>
+    <div className={'rating-star '+activeLeftClassOptions.join(" ")}>
+        <label className='star-wrapper top-left' onMouseEnter={()=>mouseEnterEvent(index+0.5)} onClick={()=>mouseClickEvent(index+0.5)} >
+            <svg viewBox="0 0 25 30" className={'star-content '+activeLeftClassOptions.join(" ")}>
+                <polygon points={starPoints} />
+            </svg>
+        </label>
+        <label className='star-wrapper top-right' onMouseEnter={()=>mouseEnterEvent(index+1)} onClick={()=>mouseClickEvent(index+1)}>
+            <svg viewBox="0 0 25 30" className={'star-content '+activeRightClassOptions.join(" ") }>
+                <polygon points={starPoints} />
+                Sorry, your browser does not support inline SVG.
+            </svg>
+        </label>
+        <label className='star-wrapper'>
+            <svg viewBox="0 0 25 30" className={'star-content '+defaultClassOptions.join(" ") }>
+                <polygon points={starPoints} />
+                Sorry, your browser does not support inline SVG.
+            </svg>
+        </label>
+    </div>
+    </div>
+}
